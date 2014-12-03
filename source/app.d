48ffb59d5b262bd9265acab4f7dbd81f9a5c9aa7
Andreas Zwinkau <zwinkau@kit.edu>
2014-12-03 15:43:19 +0100

    session management

--- source/app.d
+++ source/app.d
@@ -10,12 +10,22 @@ shared static this()
 	auto router = new URLRouter;
 	router
 		.get("/", &index)
-		.get("*", serveStaticFiles("./public/"))
+	;
+
+	auto fsettings = new HTTPFileServerSettings;
+	fsettings.serverPathPrefix = "/static";
+	router.get("/static/*", serveStaticFiles("./public/", fsettings));
+
+	/* protected sites below */
+	router
+		.any("*", &checkLogin)
+		.get("/protect", &protect)
 	;
 
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.sessionStore = new MemorySessionStore;
 	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
@@ -25,4 +35,36 @@ void index(HTTPServerRequest req, HTTPServerResponse res)
 {
 	auto pageTitle = "Prema Prediction Market";
 	res.render!("index.dt", pageTitle, req);
+}
+
+void protect(HTTPServerRequest req, HTTPServerResponse res)
+{
+	auto pageTitle = "Internal Prema Prediction Market";
+	res.render!("index.dt", pageTitle, req);
+}
+
+void login(HTTPServerRequest req, HTTPServerResponse res)
+{
+	enforceHTTP("username" in req.form && "password" in req.form,
+			HTTPStatus.badRequest, "Missing username/password field.");
+
+	// TODO verify user/password here
+
+	auto session = res.startSession();
+	session.set("username", req.form["username"]);
+	session.set("password", req.form["password"]);
+	res.redirect("/");
+}
+
+void logout(HTTPServerRequest req, HTTPServerResponse res)
+{
+	res.terminateSession();
+	res.redirect("/");
+}
+
+void checkLogin(HTTPServerRequest req, HTTPServerResponse res)
+{
+	// force a redirect to / for unauthenticated users
+	if (req.session)
+		res.redirect("/");
 }
