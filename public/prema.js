54eb8d0333301aa13e718dc7239c06cb98fbee9b
Andreas Zwinkau <qznc@web.de>
2015-11-14 22:26:50 +0100

    Show price and future chance

--- public/prema.js
+++ public/prema.js
@@ -36,5 +36,45 @@
 			tt.innerHTML = relDate(new Date(date));
 		}
 	}
+	var b = 100.0; // NOTE must match source/model.d
+	function LMSR_C(yes, no) {
+		return b * Math.log(Math.exp(yes/b) + Math.exp(no/b));
+	}
+	function LMSR_cost(yes, no, amount) {
+		return LMSR_C(yes+amount, no) - LMSR_C(yes, no);
+	}
+	function LMSR_chance(yes, no) {
+		var y =  LMSR_cost(yes, no, 1);
+		var n =  LMSR_cost(no, yes, 1);
+		return y / (y+n);
+	}
+	function roundDigits(value,digits) {
+		var factor = Math.pow(10.0,digits);
+		return Math.round(value*factor)/factor;
+	}
+	function costUpdates() {
+		var share_amount = document.getElementById("share_amount");
+		if (!share_amount) return; // cannot buy shares on this page
+		var type = document.getElementById("share_type");
+		var yes = parseInt(document.getElementById("yes_shares").innerHTML);
+		var no = parseInt(document.getElementById("no_shares").innerHTML);
+		function doUpdate() {
+			var amount = parseInt(share_amount.value);
+			var price = document.getElementById("price");
+			var future_chance = document.getElementById("future_chance");
+			if (share_type.value == "yes") {
+				price.innerHTML = roundDigits(LMSR_cost(yes,no,amount),2)+"€";
+				future_chance.innerHTML = Math.round(LMSR_chance(yes+amount,no)*100)+"%";
+			} else {
+				price.innerHTML = roundDigits(LMSR_cost(no,yes,amount),2)+"€";
+				future_chance.innerHTML = Math.round(LMSR_chance(yes,no+amount)*100)+"%";
+			}
+		}
+		share_amount.onkeyup = doUpdate;
+		share_amount.onchange = doUpdate;
+		type.onchange = doUpdate;
+		doUpdate(); // once initially
+	}
 	showRelativeTimes();
+	costUpdates();
 })();
